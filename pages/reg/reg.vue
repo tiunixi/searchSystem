ebe68bd478b02392c6bd253dd70f26ec3f1c7290
yuxin <tianyuxin_i@didichuxing.com>
2020-05-03 17:31:14 +0800

    注册修改完毕

--- pages/reg/reg.vue
+++ pages/reg/reg.vue
@@ -17,7 +17,7 @@
 				<m-input type="password" displayable v-model="password" placeholder="请输入密码"></m-input>
 				<!-- <m-input type="primary" class="clear">x</m-input> -->
 			</view>
-			
+
 		</view>
 		<view class="btn-row">
 			<button type="primary" class="primary" @tap="register">注册</button>
@@ -39,18 +39,44 @@
 				vercode: '',
 				password: '',
 				email: 'zepuer@163.com',
-				sid:'',
-				setTimeNumber:'验证码'
+				sid: '',
+				setTimeNumber: '验证码',
+				verifyShow: true,
 			}
 		},
 		methods: {
 			/*
-			* 验证码
-			*/
-			
+			 * 60S 倒计时
+			 */
+			timedown(num) {
+				let that = this;
+				if (num == 0) {
+					that.verifyShow = true; // 是否显示获取验证码
+					that.setTimeNumber = '验证码'
+					return clearTimeout();
+				} else {
+					that.verifyShow = false; // 是否显示获取验证码
+					setTimeout(function() {
+						that.setTimeNumber = (num - 1) + 's'
+						that.timedown(num - 1);
+					}, 1000); //定时每秒减一  
+				}
+			},
+
+			/*
+			 * 验证码
+			 */
+
 			getVercode() {
+				if (!this.verifyShow) {
+					uni.showToast({
+						icon: 'none',
+						title: '请稍后'
+					});
+					return
+				}
 				var pattern = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/;
-				
+
 				if (!pattern.test(this.email)) {
 					uni.showToast({
 						icon: 'none',
@@ -58,27 +84,33 @@
 					});
 					return;
 				}
-			
-				var newEmail ={email : 'this.email'}
+				var newEmail = {
+					email: this.email
+				}
+				uni.showToast({
+					icon: 'none',
+					title: '发送中，请稍后'
+				});
 				uni.request({
-					url:BASE_URL + "index/index/sendMail",  
-					data: newEmail,  
-					method:'GET',  
-					success: (e) =>{  
-						console.log(e) 
-						 if (e.statusCode === 200) {
+					url: BASE_URL + "index/index/sendMail",
+					data: newEmail,
+					method: 'GET',
+					success: (e) => {
+						console.log(e)
+						if (e.statusCode === 200) {
 							if (e.data.code === 200) {
 								console.log(e.data.data.sid)
 								this.sid = e.data.data.sid
-							}else{
+								this.timedown(60)
+							} else {
 								uni.showToast({
 									icon: 'none',
 									title: '请换个邮箱地址',
 									duration: 1000
 								});
 							}
-						 }
-					},  
+						}
+					},
 				})
 			},
 			register() {
@@ -86,10 +118,10 @@
 				 * 客户端对账号信息进行一些必要的校验。
 				 * 实际开发中，根据业务需要进行处理，这里仅做示例。
 				 */
-				if (this.vercode.length < 5) {
+				if (this.vercode.length < 4) {
 					uni.showToast({
 						icon: 'none',
-						title: '账号最短为 5 个字符'
+						title: '验证码最短为 4 个字符'
 					});
 					return;
 				}
@@ -100,13 +132,6 @@
 					});
 					return;
 				}
-				// if (this.email.length < 3 || !~this.email.indexOf('@')) {
-				// 	uni.showToast({
-				// 		icon: 'none',
-				// 		title: '邮箱地址不合法'
-				// 	});
-				// 	return;
-				// }
 
 				const data = {
 					vercode: this.vercode,
@@ -114,49 +139,45 @@
 					email: this.email,
 					sid: this.sid,
 				}
-				uniRequest.post(BASE_URL + "index/index/reg", data)
-					.then(function(response) {
-						console.log(response)
-						if (response.status === 200 && response.data.code === 200) {
-							// 登录成功后存入缓存数据addUser 
+				var that = this
+				uniRequest.post(BASE_URL + "index/index/reg", data).then(function(response) {
+					console.log(response)
+					if (response.status === 200) {
+						if (response.data.code === 200) {
+							// 登录成功后存入缓存数据addUser
 							const newData = {
-								account: data.account,
-								pwd: data.pwd,
-								s_id: response.data.data.sid
+								account: that.email,
+								pwd: that.password,
+								s_id: that.sid
 							}
 							uni.showToast({
 								icon: 'none',
 								title: '登陆成功',
 								duration: 1000
 							});
-							that.toMain(data.account);
 							service.addUser(newData)
+							console.log(1)
 							uni.reLaunch({
 								url: '../main/main',
 							});
-						
 						} else {
-							
 							uni.showToast({
 								icon: 'none',
-								title: '用户账号或密码不正确',
+								title: response.data.data.msg,
 								duration: 1000
 							});
 						}
-					}).catch(function(error) {
-					
-						console.log(error);
-					});
-				service.addUser(data);
-				that.toMain(data.account);
-				uni.showToast({
-					title: '注册成功'
-				});
-				// uni.navigateBack({
-				// 	delta: 1
-				// });
-				uni.reLaunch({
-					url: '../main/main',
+					} else {
+
+						uni.showToast({
+							icon: 'none',
+							title: '用户账号或密码不正确',
+							duration: 1000
+						});
+					}
+				}).catch(function(error) {
+
+					console.log(error);
 				});
 			}
 		}
@@ -164,19 +185,20 @@
 </script>
 
 <style lang="less" scoped>
-	
-.clear{
-	float: right;
-	display: none;
-}
-.input-row{
-	line-height: 80upx;
-	height: 80upx;
-}
-.get-vercode{
-	font-size: 24upx;
-	line-height: 80upx;
-	width: 120upx;
-	padding: 0 2upx;
-}
+	.clear {
+		float: right;
+		display: none;
+	}
+
+	.input-row {
+		line-height: 80upx;
+		height: 80upx;
+	}
+
+	.get-vercode {
+		font-size: 24upx;
+		line-height: 80upx;
+		width: 120upx;
+		padding: 0 2upx;
+	}
 </style>
